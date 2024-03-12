061ecd822a194a5b5305fcf8c3b32e08840affe7
yup50 <147809042+yup50@users.noreply.github.com>
2024-03-12 10:38:03 +0100

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * De database verbindingsgegevens
+ */
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'mvcframework-2309a');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+
+/**
+ * De naam van de virtualhost
+ */
+define('URLROOT', 'http://www.mvc-framework-2309a.org');
+
+
+/**
+ * Het pad naar de folder app
+ */
+define('APPROOT', dirname(dirname(__FILE__)));
+
---
+++ app/views/homepages/index.php
@@ -0,0 +1,10 @@
+<?php require_once APPROOT . '/views/includes/header.php'; ?>
+
+<h3><?php echo $data['title']; ?></h3>
+
+<a href="<?= URLROOT; ?>/instructeurs/index">Instructeurs</a> |
+<a href="<?= URLROOT; ?>/Countries/index">Landen van de Wereld</a> |
+
+
+
+<?php require_once APPROOT . '/views/includes/footer.php'; ?>
